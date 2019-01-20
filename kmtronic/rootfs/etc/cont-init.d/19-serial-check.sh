e6037d92ea59d7dce9a93b098aaf0cbf71a40f7d
Donat Marko <marko.donat@gmail.com>
2019-01-21 00:27:08 +0100

    :ambulance: Fixes the exception upon starting with empty serial port setting

--- kmtronic/rootfs/etc/cont-init.d/19-serial-check.sh
+++ kmtronic/rootfs/etc/cont-init.d/19-serial-check.sh
@@ -4,28 +4,29 @@ source /usr/lib/hassio-addons/base.sh
 
 PORT=$(hass.config.get 'serial_port')
 
-if [ -r ${PORT} ]; then
-    hass.log.info "Serial port (${PORT}) exists."
+if ! hass.config.has_value 'serial_port'; then
+    hass.log.warning 'No serial port was set!'
 else
-    hass.log.warning '--------------------------------------------------'
-    if ! hass.config.has_value 'serial_port'; then
-        hass.log.warning 'No serial port was set!'
-    else
+    if [ ! -r ${PORT} ]; then
         hass.log.warning "The given serial port (${PORT}) does not exist."
+    else
+        hass.log.info "Serial port (${PORT}) exists."
+        exit 0
     fi
-    hass.log.warning '--------------------------------------------------'
-    hass.log.warning 'List of available serial ports:'
-
-    find /dev -regex "\/dev\/ttyACM[0-9]" | while read line; do
-        hass.log.warning '     ' ${line}
-    done
-    find /dev -regex "\/dev\/ttyAMA[0-9]" | while read line; do
-        hass.log.warning '     ' ${line}
-    done
-    find /dev -regex "\/dev\/ttyUSB[0-9]" | while read line; do
-        hass.log.warning '     ' ${line}
-    done
-    hass.log.warning '--------------------------------------------------'
-
-    hass.die 'Save the chosen port and restart the add-on!'
 fi
+
+hass.log.warning '--------------------------------------------------'
+hass.log.warning 'List of available serial ports:'
+
+find /dev -regex "\/dev\/ttyACM[0-9]" | while read line; do
+    hass.log.warning '     ' ${line}
+done
+find /dev -regex "\/dev\/ttyAMA[0-9]" | while read line; do
+    hass.log.warning '     ' ${line}
+done
+find /dev -regex "\/dev\/ttyUSB[0-9]" | while read line; do
+    hass.log.warning '     ' ${line}
+done
+hass.log.warning '--------------------------------------------------'
+
+hass.die 'Save the chosen port and restart the add-on!'
